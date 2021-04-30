dee9c24f30035899f62d41b33bb68dff911c80ce
Joseph Abraham <ekurubal24@gmail.com>
2021-04-30 19:48:19 +0300

    Created new files, added export and import

---
+++ app.js
@@ -0,0 +1,5 @@
+import {numbersAdd, numbersDivision, sayHello} from './utils.js';
+
+numbersAdd(5, 7);
+numbersDivision(12, 3);
+sayHello('Eric');
---
+++ utils.js
@@ -0,0 +1,25 @@
+class Student{
+    constructor(name, id) {
+        this.name =ad;
+        this.id = id;
+    }
+}
+
+const PI = 3.14;
+
+function numbersAdd(num1, num2) {
+    console.log(num1 + num2)
+}
+function numbersMultiply(num1, num2) {
+    console.log(num1 * num2)
+}
+function numbersDivision(num1, num2) {
+    console.log(num1 / num2)
+}
+
+function sayHello(names) {
+    console.log(`Wellcome ${names}`);
+}
+
+
+export { numbersAdd, numbersDivision, sayHello}
